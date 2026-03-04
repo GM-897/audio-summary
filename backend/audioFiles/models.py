9b1695ff99476b5bbfb35bb59620a1a6c56b4665
manmohit <manmohitgrandhi@gmail.com>
2026-03-04 22:24:04 +0530

    add: staus and transcription fields added in the model

--- backend/audioFiles/models.py
+++ backend/audioFiles/models.py
@@ -4,8 +4,20 @@ from django.conf import settings
 # Create your models here.
 
 class AudioFile(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    )
+
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     filename = models.CharField(max_length=255, blank=True)
     file_url = models.URLField(max_length=1024, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    transcription_text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.filename.name}"
 
